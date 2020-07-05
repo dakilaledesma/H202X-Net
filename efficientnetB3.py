5f78621a80fc627e7954d4aa37a75e5b03adf8d4
bgq527 <bgq527@mocs.utc.edu>
2020-07-05 15:20:44 -0400

    Commit testing scripts

--- efficientnetB3.py
+++ efficientnetB3.py
@@ -1,6 +1,9 @@
-from tensorflow.keras.applications.nasnet import NASNetLarge
+import efficientnet.tfkeras
+from tensorflow.keras.models import load_model
+
+import efficientnet.keras as efn
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.nasnet import preprocess_input
+# from tensorflow.keras.applications.nasnet import preprocess_input
 from tensorflow.keras.utils import to_categorical
 from sklearn.utils import shuffle
 import tensorflow as tf
@@ -22,27 +25,29 @@ class Custom_Generator(tf.keras.utils.Sequence):
 
         return_x = []
         for file_name in batch_x:
-            img = image.load_img(file_name, target_size=(331, 331))
+            img = image.load_img(file_name, target_size=(320, 320))
             x = image.img_to_array(img)
-            x = preprocess_input(x)
+            x = efn.preprocess_input(x)
             return_x.append(x)
         return_x = np.array(return_x)
 
         return return_x, np.array(batch_y)
 
 
-batch_size = 4
-image_fp = np.load("data/image_fps_5ka.npy")
-labels = np.load("data/labels_5ka.npy")
+batch_size = 8
+image_fp = np.load("data/image_fps.npy")
+labels = np.load("data/labels.npy")
 labels = to_categorical(labels, dtype=np.bool)
 
 image_fp, labels = shuffle(image_fp, labels)
 train_gen = Custom_Generator(image_fp, labels, batch_size)
 
-model = NASNetLarge(weights=None, include_top=True, input_shape=(331, 331, 3), classes=5000)
-model.compile(optimizer="adam", loss="categorical_crossentropy")
-model.fit_generator(generator=train_gen,
-                    steps_per_epoch=int(image_fp.shape[0] // batch_size),
-                    epochs=10,
-                    verbose=1)
-model.save("models\\nasnetl-1")
+# model = efn.EfficientNetB3(weights=None, include_top=True, input_shape=(320, 320, 3), classes=32094)
+for l in range(10, 30):
+    model = load_model(f"models\\efficientnetb3-1-{l}-full")
+    model.compile(optimizer="adam", loss="categorical_crossentropy")
+    model.fit_generator(generator=train_gen,
+                        steps_per_epoch=int(image_fp.shape[0] // batch_size),
+                        epochs=1,
+                        verbose=1)
+    model.save(f"models\\efficientnetb3-1-{l + 1}--full")
