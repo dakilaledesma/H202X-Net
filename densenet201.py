86ec6561506b596c343e9c04c26c52e90d406efa
Dakila Ledesma <37383659+bgq527@users.noreply.github.com>
2020-08-13 16:46:12 -0400

    Update densenet201.py

--- densenet201.py
+++ densenet201.py
@@ -31,7 +31,7 @@ class Custom_Generator(keras.utils.Sequence):
 
         return_x = []
         for file_name in batch_x:
-            img = image.load_img(file_name, target_size=(224, 224))
+            img = image.load_img(file_name, target_size=(224, round(224 * 1.46)))
             x = image.img_to_array(img)
             x = preprocess_input(x)
             return_x.append(x)
@@ -132,7 +132,7 @@ class AdamAccumulate(Optimizer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
-batch_size = 2
+batch_size = 4
 image_fp = np.load("data/image_fps.npy")
 labels = np.load("data/labels.npy")
 labels = to_categorical(labels, dtype=np.bool)
@@ -140,13 +140,13 @@ labels = to_categorical(labels, dtype=np.bool)
 image_fp, labels = shuffle(image_fp, labels)
 train_gen = Custom_Generator(image_fp, labels, batch_size)
 
-acc_opt = AdamAccumulate(lr=0.001, decay=1e-5, accum_iters=128)
+acc_opt = AdamAccumulate(lr=0.001, decay=1e-5, accum_iters=64)
 
 steps = int(image_fp.shape[0] // batch_size)
-model = DenseNet201(weights=None, include_top=True, input_shape=(224, 224, 3), classes=32094)
+model = DenseNet201(weights=None, include_top=True, input_shape=(224, round(224 * 1.46), 3), classes=32094)
 model.compile(optimizer=acc_opt, loss="categorical_crossentropy")
 model.fit_generator(generator=train_gen,
                     steps_per_epoch=int(image_fp.shape[0] // batch_size),
                     epochs=10,
                     verbose=1)
-model.save("models\\densenet201-1")
+model.save("models\\densenet201-2")
