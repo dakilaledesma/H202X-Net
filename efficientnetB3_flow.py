e26d1c5291c695fec6974fdf484a76f945d3fd66
Dakila Ledesma <37383659+bgq527@users.noreply.github.com>
2020-10-07 16:10:52 -0400

    new resolutions to see if wrong resolution actually better

--- efficientnetB3_flow.py
+++ efficientnetB3_flow.py
@@ -127,7 +127,7 @@ file_df = pd.DataFrame(list(zip(image_fp, labels)), columns=["filename", "class"
 print(file_df.head())
 
 datagen = image.ImageDataGenerator(horizontal_flip=True, zoom_range=[0.85, 0.85], preprocessing_function=efn.preprocess_input)
-train_gen = datagen.flow_from_dataframe(file_df, target_size=(500, 340), shuffle=True, class_mode="categorical", batch_size=batch_size)
+train_gen = datagen.flow_from_dataframe(file_df, target_size=(320, 320), shuffle=True, class_mode="categorical", batch_size=batch_size)
 pickled_classes = open('eb3_traingen_classes', 'wb')
 pickle.dump(train_gen.class_indices, pickled_classes)
 pickled_classes.close()
@@ -140,7 +140,7 @@ https://stackoverflow.com/questions/37340129/tensorflow-training-on-my-own-image
 acc_opt = AdamAccumulate(lr=0.001, decay=1e-5, accum_iters=64)
 
 model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
-    filepath="cp/efficientnetb3-5-bottleneck-{epoch:02d}",
+    filepath="cp/efficientnetb3-6-{epoch:02d}",
     save_weights_only=False,
     monitor='loss',
     mode='min',
@@ -149,20 +149,20 @@ model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
 '''
 Load model
 '''
-model = load_model("cp/efficientnetb3-5-bottleneck-01", custom_objects={'AdamAccumulate': AdamAccumulate}, compile=False)
+# model = load_model("cp/efficientnetb3-5-bottleneck-01", custom_objects={'AdamAccumulate': AdamAccumulate}, compile=False)
 
 '''
 Without bottleneck
 '''
-# model = efn.EfficientNetB3(weights=None, include_top=True, input_shape=(500, 340, 3), classes=32093)
-# en_model = efn.EfficientNetB3(weights='noisy-student', include_top=False, input_shape=(500, 340, 3), pooling='avg')
-# model_output = Dense(32093, activation='softmax')(en_model.output)
-# model = Model(inputs=en_model.input, outputs=model_output)
+# model = efn.EfficientNetB3(weights=None, include_top=True, input_shape=(320, 320, 3), classes=32093)
+en_model = efn.EfficientNetB3(weights='noisy-student', include_top=False, input_shape=(320, 320, 3), pooling='avg')
+model_output = Dense(32093, activation='softmax')(en_model.output)
+model = Model(inputs=en_model.input, outputs=model_output)
 
 '''
 With bottleneck
 '''
-# en_model = efn.EfficientNetB3(weights='noisy-student', include_top=False, input_shape=(500, 340, 3), pooling='avg')
+# en_model = efn.EfficientNetB3(weights='noisy-student', include_top=False, input_shape=(320, 320, 3), pooling='avg')
 # model_output = Dense(512, activation='relu')(en_model.output)
 # model_output = Dense(32093, activation='softmax')(model_output)
 # model = Model(inputs=en_model.input, outputs=model_output)
@@ -179,4 +179,4 @@ model.fit_generator(generator=train_gen,
                     callbacks=[model_checkpoint_callback,
                                CosineAnnealingScheduler(T_max=2, eta_max=1e-2, eta_min=1e-4)])
 
-model.save("models\\efficientnetb3-5-bottleneck")
+model.save("models\\efficientnetb3-6")
