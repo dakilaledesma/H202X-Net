9c4283017fd481d94c31d57416415d28af45598c
Dakila Ledesma <37383659+bgq527@users.noreply.github.com>
2020-10-13 15:46:57 -0400

    Update efficientnetB3_flow.py

--- efficientnetB3_flow.py
+++ efficientnetB3_flow.py
@@ -92,6 +92,6 @@ model.fit(tfds,
                     steps_per_epoch=int(image_fp.shape[0] // batch_size),
                     epochs=12,
                     verbose=1,
-                    callbacks=[model_checkpoint_callback], max_queue_size=30, workers=16, use_multiprocessing=True)
+                    callbacks=[model_checkpoint_callback], max_queue_size=100, workers=32, use_multiprocessing=True)
 
 model.save("models\\efficientnetb3-6")
