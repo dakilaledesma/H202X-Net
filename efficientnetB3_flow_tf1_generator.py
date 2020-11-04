5b77de0bfcecb21b8e080e5927e3d95cfc1badac
Dakila Ledesma <37383659+bgq527@users.noreply.github.com>
2020-11-04 12:18:44 -0500

    no shuffle try

--- efficientnetB3_flow_tf1_generator.py
+++ efficientnetB3_flow_tf1_generator.py
@@ -64,7 +64,7 @@ def train_preprocess(image, label):
 
 
 tfds = tf.data.Dataset.from_generator(generator, output_types=(tf.string, tf.float32),
-                                      output_shapes=(None, [32094])).shuffle(len(image_fp))
+                                      output_shapes=(None, [32094]))
 tfds = tfds.map(parse_function, num_parallel_calls=20).map(train_preprocess, num_parallel_calls=20)
 tfds = tfds.batch(batch_size)
 tfds = tfds.prefetch(10)
