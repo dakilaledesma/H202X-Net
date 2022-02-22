5380ef30cd5aab8559658981fd9780d97619e230
Dax Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2022-02-22 17:27:09 -0500

    Update checkpoint_saver.py

--- timm/utils/checkpoint_saver.py
+++ timm/utils/checkpoint_saver.py
@@ -65,8 +65,8 @@ class CheckpointSaver:
         tmp_save_path = os.path.join(self.checkpoint_dir, 'tmp' + self.extension)
         last_save_path = os.path.join(self.checkpoint_dir, 'last' + self.extension)
         self._save(tmp_save_path, epoch, metric)
-        if os.path.exists(last_save_path):
-            os.unlink(last_save_path)  # required for Windows support.
+        # if os.path.exists(last_save_path):
+            # os.unlink(last_save_path)  # required for Windows support.
         os.rename(tmp_save_path, last_save_path)
         worst_file = self.checkpoint_files[-1] if self.checkpoint_files else None
         if (len(self.checkpoint_files) < self.max_history
@@ -75,7 +75,7 @@ class CheckpointSaver:
                 self._cleanup_checkpoints(1)
             filename = '-'.join([self.save_prefix, str(epoch)]) + self.extension
             save_path = os.path.join(self.checkpoint_dir, filename)
-            os.link(last_save_path, save_path)
+            # os.link(last_save_path, save_path)
             self.checkpoint_files.append((save_path, metric))
             self.checkpoint_files = sorted(
                 self.checkpoint_files, key=lambda x: x[1],
@@ -92,7 +92,7 @@ class CheckpointSaver:
                 best_save_path = os.path.join(self.checkpoint_dir, 'model_best' + self.extension)
                 if os.path.exists(best_save_path):
                     os.unlink(best_save_path)
-                os.link(last_save_path, best_save_path)
+                # os.link(last_save_path, best_save_path)
 
         return (None, None) if self.best_metric is None else (self.best_metric, self.best_epoch)
 
